eca8069a6976b36304c553d04bec4316bd0555b9
Tushar Patel <tusharspatel1@gmail.com>
2023-04-01 16:34:30 -0400

    parts 1 & 2 lab 3

--- Lab3/part2/part2.ino
+++ Lab3/part2/part2.ino
@@ -1,8 +1,8 @@
 
 int photoPin = A0;
 
-int redPin = 2;
-int greenPin = 4;
+int redPin = 10;
+int greenPin = 9;
 int bluePin = 3;
 
 int trigPin = 5;
@@ -34,35 +34,49 @@ void loop() {
 
   //Serial.println(scaled_input);
 
-  analogWrite(bluePin, photo_input);
+//  analogWrite(bluePin, photo_input);
 
 
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
 
+
+
+  
+
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-
+  
   float duration = pulseIn(echoPin, HIGH);
   float distance = (duration / 2)*0.0344;
-  int int_distance = min(distance, 255); 
+  distance = distance * 4;
+  int int_distance =min(distance, 255); 
 
 
   Serial.println(int_distance);
 
-  for(int i = 0; i < 255; i ++){
-    analogWrite(greenPin, i);
-    //analogWrite(greenPin, 255 - i);
-    delay(10);
-  }
+  // for(int i = 100; i < 150; i ++){
+  //   analogWrite(redPin, i);
+    
+  //   analogWrite(bluePin, 255 - i);
 
-  //analogWrite(redPin, int_distance);
- // analogWrite(greenPin, 255 - int_distance);
+  //   Serial.println(i);
+  //   delay(100);
+  // }
+
+   analogWrite(redPin, int_distance);
+   analogWrite(greenPin, 255 - int_distance);
 
   delay(1000);
 
 }
+
+// void loop(){
+//   analogWrite(greenPin, 200);
+//   analogWrite(redPin, 255);
+//   analogWrite(bluePin, 120);
+// }
 
 
